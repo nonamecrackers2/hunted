864e33f814376e7cfebc2812c00a9a1021d242ae
nonamecrackers2 <105086648+nonamecrackers2@users.noreply.github.com>
2022-10-22 11:05:53 -0600

    Fix long player names extending beyond render bounds

--- src/main/java/nonamecrackers2/hunted/client/gui/component/QueuedPlayerList.java
+++ src/main/java/nonamecrackers2/hunted/client/gui/component/QueuedPlayerList.java
@@ -1,5 +1,6 @@
 package nonamecrackers2.hunted.client.gui.component;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -7,6 +8,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.client.gui.components.PlayerFaceRenderer;
@@ -14,6 +16,7 @@ import net.minecraft.client.multiplayer.PlayerInfo;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.FormattedCharSequence;
 import nonamecrackers2.hunted.huntedclass.HuntedClass;
 import nonamecrackers2.hunted.init.HuntedClassTypes;
 import nonamecrackers2.hunted.util.HuntedClassSelector;
@@ -75,15 +78,29 @@ public class QueuedPlayerList extends ObjectSelectionList<QueuedPlayerList.Entry
 		@Override
 		public void render(PoseStack stack, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean selected, float partialTicks)
 		{
+			Font font = QueuedPlayerList.this.minecraft.font;
 			RenderSystem.setShaderTexture(0, this.skinGetter.get());
 			PlayerFaceRenderer.draw(stack, left + 4, top + (height - FACE_SIZE) / 2, FACE_SIZE);
-			MutableComponent name = Component.literal(this.name);
+			Component name = Component.literal(this.name);
+			MutableComponent suffixWidthTester = Component.literal("...");
+			Component vip = Component.literal(" VIP").withStyle(ChatFormatting.GREEN);
 			if (this.isVip)
-				name.append(Component.literal(" VIP").withStyle(ChatFormatting.GREEN));
-			GuiComponent.drawString(stack, QueuedPlayerList.this.minecraft.font, name, left + 8 + FACE_SIZE, top + 2, 16777215);
+				suffixWidthTester.append(vip);
+			List<FormattedCharSequence> text = font.split(name, width - 8 - FACE_SIZE - font.width(suffixWidthTester));
+			if (text.size() > 0)
+			{
+				GuiComponent.drawString(stack, font, text.get(0), left + 8 + FACE_SIZE, top + 2, 16777215);
+				MutableComponent suffix = Component.empty();
+				if (text.size() > 1)
+					suffix.append(Component.literal("..."));
+				if (this.isVip)
+					suffix.append(vip);
+				GuiComponent.drawString(stack, font, suffix, left + 8 + FACE_SIZE + font.width(text.get(0)), top + 2, 16777215);
+					
+			}
 			HuntedClass huntedClass = this.selector.getFromType(HuntedClassTypes.PREY.get());
 			Component prey = Component.translatable(huntedClass.id().getNamespace() + ".class." + huntedClass.id().getPath()).withStyle(ChatFormatting.DARK_GRAY);
-			GuiComponent.drawString(stack, QueuedPlayerList.this.minecraft.font, prey, left + 8 + FACE_SIZE, top + 4 + QueuedPlayerList.this.minecraft.font.lineHeight, 16777215);
+			GuiComponent.drawString(stack, font, prey, left + 8 + FACE_SIZE, top + 4 + font.lineHeight, 16777215);
 		}
 	}
 }
