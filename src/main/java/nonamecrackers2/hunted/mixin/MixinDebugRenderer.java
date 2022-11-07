a8fe73988942420d7e419a3c1ec4ddc98739cd3f
nonamecrackers2 <105086648+nonamecrackers2@users.noreply.github.com>
2022-11-06 21:04:38 -0600

    Toggle pathfinding renderer

--- src/main/java/nonamecrackers2/hunted/mixin/MixinDebugRenderer.java
+++ src/main/java/nonamecrackers2/hunted/mixin/MixinDebugRenderer.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.debug.DebugRenderer;
 import net.minecraft.client.renderer.debug.PathfindingRenderer;
@@ -24,6 +25,8 @@ public abstract class MixinDebugRenderer
 	)
 	public void renderTail(PoseStack stack, MultiBufferSource.BufferSource buffer, double x, double y, double z, CallbackInfo ci)
 	{
-		this.pathfindingRenderer.render(stack, buffer, x, y, z);
+		Minecraft mc = Minecraft.getInstance();
+		if (mc.getEntityRenderDispatcher().shouldRenderHitBoxes())
+			this.pathfindingRenderer.render(stack, buffer, x, y, z);
 	}
 }
